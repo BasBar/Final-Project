a524542ec64d0ba4768ae5906c920bf419191680
BasBar <basarabartlomiej@gmail.com>
2019-10-06 12:34:18 +0200

    Modified ReservationServiceTest

--- src/test/java/com/sda/restaurant/serviceTest/ReservationServiceTest.java
+++ src/test/java/com/sda/restaurant/serviceTest/ReservationServiceTest.java
@@ -8,6 +8,7 @@ import com.sda.restaurant.repositories.ClientRepository;
 import com.sda.restaurant.repositories.ReservationRepository;
 import com.sda.restaurant.repositories.TableRepository;
 import com.sda.restaurant.services.ReservationService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -38,19 +39,24 @@ public class ReservationServiceTest {
     @InjectMocks
     ReservationService reservationService;
 
+    Set<Tables> tablesSet;
+    Tables tables1;
+    ReservationForm reservationForm;
+    Reservation reservation;
+
+    @Before
+    public void setUp() {
+
+        tablesSet = new HashSet<>();
+        tables1 = new Tables();
+        tablesSet.add(tables1);
+        reservationForm = new ReservationForm();
+        reservationForm.setTablesId(new Long[]{1L, 2L});
+        reservation = new Reservation();
+        reservation.setId(5L);
+    }
     @Test
     public void whenSaveReservationReturnItsId(){
-
-
-        Set<Tables> tablesSet = new HashSet<>();
-        Tables tables1 = new Tables();
-        tablesSet.add(tables1);
-
-        ReservationForm reservationForm = new ReservationForm();
-        reservationForm.setTablesId(new Long[]{1L, 2L});
-        Reservation reservation = new Reservation();
-        reservation.setId(5L);
-
         when(reservationRepository.save(any())).thenReturn(reservation);
         Long created = reservationService.saveReservation(reservationForm);
         assertThat(created).isEqualTo(5L);
